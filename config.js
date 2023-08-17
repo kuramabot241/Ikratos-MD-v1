8ebeedc3c8491c33b78575a066fb765551b59843
kuramabot241 <134456649+kuramabot241@users.noreply.github.com>
2023-08-17 23:23:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -54,9 +54,9 @@ global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada 
 
 /*============== NOMOR ==============*/
 global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorown = '20222056584' //Nomor Owner
+global.namebot = 'Raizel-MD-v1' //Nama Bot
+global.nameown = 'Raizel Bot' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
